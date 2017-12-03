bef5d0c200aca37d815c9fe767b7a0247a980692
Noam Bachmann <noam@helou.ch>
2017-12-03 13:55:25 +0100

    added my test account

--- USERS.js
+++ USERS.js
@@ -6,5 +6,9 @@
   {
     "websocket": "wss://retog.solidtest.space",
     "webpage": "https://retog.solidtest.space/public/twee-fi/"
+  },
+  {
+    "websocket": "wss://noam.solidtest.space",
+    "webpage": "https://noam.solidtest.space/public/twee-fi/"
   }
 ]
