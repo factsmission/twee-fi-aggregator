a7365ddf8217634cc688d706650b13ba9d721acc
Davide Allavena <DavideAllavena@users.noreply.github.com>
2018-02-23 15:56:08 +0100

    Update USERS.js

--- USERS.js
+++ USERS.js
@@ -1,5 +1,6 @@
 [
   "https://alemela.solidtest.space/public/twee-fi/",
   "https://retog.solidtest.space/public/twee-fi/",
-  "https://noam.solidtest.space/public/twee-fi/"
+  "https://noam.solidtest.space/public/twee-fi/",
+  "https://davide.solidtest.space/public/twee-fi/"
 ]
